9e4e005790448fc0600af1c3ccd2f76f5f4bdefc
Аюпов Дима <dimaaupov2005@gmail.com>
2025-10-01 15:53:25 +0300

    added js file

---
+++ popup.js
@@ -0,0 +1,9 @@
+const saveButton = document.getElementById("save");
+saveButton.addEventListener("click",() => {    
+    alert("save");
+})
+
+const restoreButton = document.getElementById("restore");
+restoreButton.addEventListener("click",() => {    
+    alert("restore");
+})
